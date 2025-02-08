e3e282dbf451f8fc5d286db063aa1852705db91e
tangent03 <amanawp03@gmail.com>
2025-02-08 15:50:07 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -8,19 +8,23 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-
 }));
-
 app.use(express.json({
     limit: "20kb"
 }));
-
 app.use(express.urlencoded({
     extended: true,
     limit: "20kb"
 }));
-
 app.use(cookieParser());
-
 app.use(express.static("public"));
+
+
+
+//routes
+
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
 export default app;
